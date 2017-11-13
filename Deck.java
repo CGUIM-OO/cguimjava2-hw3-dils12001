7e5ea881ea0c0a770fcf59c51e44e8200aad5728
dils12001 <32242875+dils12001@users.noreply.github.com>
2017-11-13 18:32:17 +0800

    543536

---
+++ Deck.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+	private ArrayList<Card> cards;
+	private ArrayList<Card> usedCard;
+	public int nUsed;
+
+	public Deck(int nDeck) {
+		cards = new ArrayList<Card>();
+		usedCard = new ArrayList<Card>();
+
+		for (int i = 1; i <= nDeck; i++) {
+			for (Card.Suit s : Card.Suit.values()) {
+				for (int y = 1; y <= 13; y++) {
+					Card card = new Card(s, y);
+					cards.add(card);
+				}
+			}
+		}
+		shuffle();
+	}
+
+	public void printDeck() {
+
+		for (int i = 0; i < cards.size(); i++) {
+			cards.get(i).printCard();
+		}
+	}
+
+	public ArrayList<Card> getAllCards() {
+		return cards;
+	}
+
+	public void shuffle() {
+		Random rnd = new Random();
+		for (int i = 0; i < cards.size(); i++) {
+			int j = rnd.nextInt(cards.size());
+			Card a = cards.get(i);
+			Card b = cards.get(j);
+			Card temp = a;
+			a = b;
+			b = temp;
+		}
+		nUsed = 0;
+		for (int i = 0; i < usedCard.size(); i++) {
+			usedCard.remove(i);
+		}
+	}
+
+	public Card getOneCard() {
+		Card onecard = null;
+		if(usedCard.size() == cards.size())
+			{shuffle();
+		getOneCard();}
+		else
+		{onecard = cards.get(nUsed);
+		nUsed++;
+		usedCard.add(onecard);}
+		return onecard;
+	}
+}
